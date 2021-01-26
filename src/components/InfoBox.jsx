558b85db4551c16f5e75f622b0eb2f926cc0f16a
Sepehr Kashani <kashanisepehr1992@gmail.com>
2021-01-26 19:50:46 +1100

    Update InfoBox.jsx

--- src/components/InfoBox.jsx
+++ src/components/InfoBox.jsx
@@ -4,8 +4,8 @@ import numeral from 'numeral'
 
 const InfoBox = ({title, cases }) => {
   
-  const formatNumbers = (cases) => numeral((String(cases))).format('0,0')
-  
+const formatNumbers = (cases) => numeral((String(cases))).format('0,0')
+
   return (
     <div className="info-card">
       <h1 className="info-heading">{formatNumbers(cases)}</h1>
